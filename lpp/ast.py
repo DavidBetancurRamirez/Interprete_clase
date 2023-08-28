98d350ae30289fe5a32172d3bd51d6909e3d3275
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2023-08-28 15:22:07 -0500

    avance clase

--- lpp/ast.py
+++ lpp/ast.py
@@ -2,7 +2,10 @@ from abc import (
     ABC,
     abstractmethod
 )
-from typing import List
+from typing import (
+    List,
+    Optional
+)
 from lpp.tokens import Token
 
 class ASTNode(ABC):
@@ -32,13 +35,46 @@ class Program(ASTNode):
     """A Program is a list of statements"""
     def __init__(self, statements:List[Statement]) -> None:
         self.statements = statements
+
     def token_literal(self) -> str:
         if len(self.statements)>0:
             return self.statements[0].token_literal()
         return ""
+    
     def __str__(self) -> str:
         out:List[str]=[]
         for statement in self.statements:
             out.append(str(statement))
 
         return "".join(out)
+    
+class Identifier(Expression):
+    def __init__(self,token:Token,
+                 value:str)->None:
+        super().__init__(token)
+        self.value=value
+
+    def __str__(self) -> str:
+        return self.value
+    
+class LetStatement(Statement):
+    def __init__(self,
+                 token:Token,
+                 name:Optional[Identifier]=None,
+                 value:Optional[Expression]=None)->None:
+        super().__init__(token)
+        self.name=name
+        self.value=value
+
+    def __str__(self) -> str:
+        return f'{self.token_literal()} {str(self.name)}={str(self.value)}'
+    
+class ReturnStatements(Statement):
+    def __init__(self, token: Token,
+                    return_value:Optional[Expression]=None) -> None:
+        super().__init__(token)
+        self.return_value=return_value
+
+    def __str__(self) -> str:
+        return f'{self.token_literal()} {str(self.return_value)}'
+    
--- lpp/parser.py
+++ lpp/parser.py
@@ -1,11 +1,91 @@
-from lpp.ast import Program
+from typing import (
+    Optional,
+    List
+)
+from lpp.ast import (
+    Expression,
+    Identifier,
+    LetStatement,
+    ReturnStatements,
+    Statement,
+    Program
+)
+from lpp.tokens import (
+    Token,
+    TokenType
+)
 from lpp.lexer import Lexer
 
 class Parser:
     def __init__(self, lexer:Lexer) -> None:
         self._lexer = lexer
+        self._current_token:Optional[Token]=None
+        self._peek_token:Optional[Token]=None
+        self._errors:List[str]=[]
+
+        self._advance_tokens()
+
+    @property
+    def errors(self)->None:
+        return self._errors
+    
+    def _expectet_token_error(self,token_type=TokenType)->None:
+        assert self._peek_token is not None
+        error=f'Se esperaba {token_type} y se obtuvo {self._peek_token.token_type}'
+        self._errors.append(error)
 
     def parse_program(self) -> Program:
         program : Program = Program(statements=[])
-
         return program
+    
+    def _expectet_token(self,token_type:TokenType)->bool:
+        assert self._peek_token is not None
+        if self._peek_token==token_type:
+            self._advance_tokens()
+            return True
+        # Para agregar el error en el token
+        self._expectet_token_error(token_type)
+        return False
+    
+    def _advance_tokens(self)->None:
+        self._current_token=self._peek_token
+        self._peek_token=self._lexer.next_token()
+
+    def _parse_let_statement(self)->Optional[LetStatement]:
+        assert self._current_token is not None
+        let_statement:LetStatement=LetStatement(token=self._current_token)
+ 
+        if not self._expected_token(TokenType.IDENT):
+            return None
+        
+        let_statement.name=Identifier(token=self._current_token,value=self._current_token)
+ 
+        if not self._expected_token(TokenType.ASSING):
+            return None
+        
+        #Por hacer cuando sepamos transformar guardar los valores
+ 
+        while self._current_token.token_type != TokenType.SEMICOLON:
+            self._advance_tokens()
+ 
+        return let_statement
+    
+    def _parse_return_statement(self)->Optional[ReturnStatements]:
+        assert self._current_token is not None
+        return_statement=ReturnStatements(token=self._current_token)
+        #Cuando sepamos evaluar expresionaes para retornar
+        self._advance_tokens()
+        while self._current_token.token_type!=TokenType.SEMICOLON:
+            self._advance_tokens()
+
+        return return_statement
+
+    def _parse_statement(self)->Optional[Statement]:
+        assert self._current_token is not None
+        if self._current_token.token_type==TokenType.LET:
+            return self._parse_let_statement()
+        elif self._current_token.token_type==TokenType.RETURN:
+            return self._parse_return_statement()
+        else:
+            None
+            
